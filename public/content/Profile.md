01b7c01b3cc520d718f50925da2c707ac88e05d1
Dorps <Dorps@users.noreply.github.com>
2023-05-13 04:15:28 -0400

    chakra ui theme installed

---
+++ public/content/Profile.md
@@ -0,0 +1,19 @@
+## Site Name
+- Portable Portfolio
+
+## Header
+- Hi, my name is Jeffrey Dang. 
+- I'm a software engineer.
+
+
+## About
+Hi, I'm a Software Engineering and Management graduate from McMaster University. Passionate about software development and gaming.
+
+## Contact
+Write your "Contact" section here.
+- LinkedIn: https://linkedin.com/in/username
+- GitHub: https://github.com/username
+- Email: username@gmail.com
+
+## Logo
+- JD
--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import logo from './logo.svg';
 import gif from './images/adventure-time-gunter.gif'
 import './App.css';
+import Nav from './components/NavBar';
+import Header from './components/Hero';
+import About from './components/About';
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <div className="App">
+      <Nav color ="cyan" />
+      <Header color="cyan" />
+      <About color="cyan" />
+      <Projects color="cyan" />
       <header className="App-header">
-        <img src={gif} className="App-logo" alt="logo" />
-        <p>
-          Jeffrey Dang - Portfolio Website 
-        </p>
-        <p>
-        </p>
+        {/* <img src={gif} className="App-logo" alt="logo" /> */}
       </header>
     </div>
   );
